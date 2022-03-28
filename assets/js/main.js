4d1e9f6189853e934b8d09377e0a038628551aff
Ivan Toselli <ivan.toselli31@gmail.com>
2022-03-28 21:32:29 +0200

    Stp 2: Exe almost complete..

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@ let start = document.getElementById('btn');
 let select = document.getElementById('difficoltà');
 // Selezioniamo <p></p> nella griglia
 let paragrafo = document.getElementById('welcome');
+let scoreboard = document.getElementById('scoreboard');
+let box;
 
 
 
@@ -28,23 +30,54 @@ start.addEventListener('click', function(){
         //Condizione legata alla difficoltà scelta
         if (select.value == "facile"){
 
-                for(let i = 1; i < 101; i++){ 
+                for(let i = 0; i < 100; i++){ 
                         // Selezioniamo la griglia
                         let grid = document.getElementById('grid');
                         // Creazione del box interno alla griglia
-                        let box = document.createElement('div');
+                        box = document.createElement('div');
                         //Inserire 100 box all'interno della griglia      
                         grid.appendChild(box);
                         //Aggiungere classe al singolo box
                         box.classList.add('box-10');
                         // Inserire numero dentro il box
-                        box.innerHTML += `<span>${(i)}</span>`;
+                        box.innerHTML += `<span>${(array[i])}</span>`;
+                        // Al click del box cambia il colore della cella diventando blu
+                        box.addEventListener('click', checkedBombedBox)
                 }
 
         }else if (select.value == "media"){
 
+                for(let i = 0; i < 81; i++){ 
+                        // Selezioniamo la griglia 9x9(81 cells)
+                        let grid = document.getElementById('grid');
+                        // Creazione del box interno alla griglia
+                        box = document.createElement('div');
+                        //Inserire 100 box all'interno della griglia      
+                        grid.appendChild(box);
+                        //Aggiungere classe al singolo box
+                        box.classList.add('box-9');
+                        // Inserire numero dentro il box
+                        box.innerHTML += `<span>${(array[i])}</span>`;
+                        // Al click del box cambia il colore della cella diventando blu
+                        box.addEventListener('click', checkedBombedBox)
+                        
+                }
         }else{
-
+                
+                for(let i = 0; i < 49; i++){ 
+                        // Selezioniamo la griglia 7x7(49 cells)
+                        let grid = document.getElementById('grid');
+                        // Creazione del box interno alla griglia
+                        box = document.createElement('div');
+                        //Inserire 100 box all'interno della griglia      
+                        grid.appendChild(box);
+                        //Aggiungere classe al singolo box
+                        box.classList.add('box-7');
+                        // Inserire numero dentro il box
+                        box.innerHTML += `<span>${(array[i])}</span>`;
+                        // Al click del box cambia il colore della cella diventando blu
+                        box.addEventListener('click', checkedBombedBox, bombedFound)
+                }
         }
         
 } )
@@ -55,16 +88,44 @@ function getRandomIntInclusive(min, max){
         return Math.floor(Math.random()* (max - min + 1) + min);
 }
 
-//console.log(getRandomIntInclusive(1,100));
 
-        let array = [];
+    let array = [];
 
-        for (y=1; y <= 100; y++){
-                let numberRando = getRandomIntInclusive(1,100);
-
-                do{
-                         array.push(numberRando)
-                }while(!array.includes(numberRando))
+        for (y=0; y <= 100; y++){
+        array.push(y);        
         }
-  
-//console.log(array);
+
+        function shuffle(array){
+                return array.sort(() => Math.random() - 0.5);
+        }
+
+
+        array = shuffle(array);
+
+//array per le bombe
+let arrayBombe = [];
+for (bombe=1; bombe < 100; bombe++){
+        arrayBombe.push(bombe);
+}
+arrayBombe = shuffle(arrayBombe);
+
+//array 16 bombe
+let arraySediciBombe = [];
+for (bombe=1; bombe < 16; bombe++){
+        arraySediciBombe.push(arrayBombe[bombe]);
+}
+console.log(arraySediciBombe);
+
+function checkedBombedBox(){
+        if (arraySediciBombe.includes(parseInt(this.innerText))){
+                this.classList.add('bomba');
+                alert(`Hai fatto esplodere una bomba, hai perso!`)
+        }else{
+                this.classList.add('clicked');
+                let point = 0;
+                point++;
+                scoreboard.innerHTML = `Il tuo punteggio è: `; 
+                scoreboard.innerHTML = `${point}`
+        }
+}
+
